60b542f6e151c64dd0e10c3611d4937474c332f5
Alex Fontanellas <alexfontanellas@hotmail.com>
2019-11-02 19:30:22 -0400

    debug

--- example 2/src/app/app.routing.ts
+++ example 2/src/app/app.routing.ts
@@ -9,7 +9,6 @@ const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
-
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
 ];
--- example 2/src/main.ts
+++ example 2/src/main.ts
@@ -1,6 +1,16 @@
 ﻿import './polyfills';
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+// const express = require('express');
+// const app = express();
+// const users_path = require('./app/users/user.controller');
+
 platformBrowserDynamic().bootstrapModule(AppModule);
+//
+// app.use('/users', users_path);
+//
+// const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+// const server = app.listen(port, function () {
+//     console.log('Server listening on port ' + port);
+// });
