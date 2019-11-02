d4d9254a926521900aa54271b9a82caa8fce26b8
adamrab123 <arabinowitz10@gmail.com>
2019-11-02 19:57:33 -0400

    grader can now mark right and wrong answers, puts percentage on image

--- reader.py
+++ reader.py
@@ -8,7 +8,7 @@ def find_largest_polygon(img):
     threshold = cv2.threshold(img, 127, 255, 0)[1]
     
 
-    _, contours, hierarchy = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(threshold, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     polygon = max(contours, key=cv2.contourArea)
     rect = cv2.minAreaRect(polygon)
     box = cv2.boxPoints(rect)
@@ -40,8 +40,10 @@ def crop_and_warp(img, crop_rect, w, h):
 
 
 
+filename = 'images/test.jpg'
+image, extension = filename.split(".")
 
-im = cv2.imread('images/test2.jpg')
+im = cv2.imread(filename)
 
 
 im_gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
@@ -67,16 +69,22 @@ print(circles)
 circles = np.round(circles[0, :]).astype(np.int32)
 circles = sorted(circles, key = lambda x: x[1])
 
+correct = {1: 'C', 2: 'D', 3: 'B', 4: 'A'}
 mapping = {0:"A", 1:"B", 2:"C", 3:"D"}
 res = dict()
 intensities = []
+locations = []
 for (x, y, r) in circles:
     print(x,y,r)
     mask = np.zeros(im_gray.shape)
-    cv2.circle(im, (x, y), r, (0,255,0), 4)
+    # cv2.circle(im, (x, y), r, (0,255,0), 4)
     cv2.circle(mask, (x, y), r, 1, -1)
     intensity = np.mean(mask * im_gray)
     intensities.append(intensity)
+    locations.append((intensity,x,y))
+plt.imshow(im)
+
+    
 
 
 # num_questions = 4
@@ -84,6 +92,53 @@ for (x, y, r) in circles:
 #     print(intensities[4*i:4*i+4])
 #     res[i+1] = mapping[np.argmin(intensities[4*i:4*i+4])]
 
-
+outputName = image + "-circled" + "." + extension
+print("writing to ", outputName)
 #cv2.polylines(im, [np.array(paper_outline).astype(np.int32)], True, (255,0,0), 2)
-cv2.imwrite("ans.jpeg", im)
+cv2.imwrite(outputName, im)
+
+
+
+# print(locations)
+num_questions = 4
+for i in range(num_questions):
+    res[i+1] = [mapping[np.argmin(intensities[4*i:4*i+4])], np.min(intensities[4*i:4*i+4])]
+
+print(res)
+print(correct)
+
+
+count = 0
+for (userAnswer,(userAnswer,userIntensity)), elem in zip(res.items(), correct):
+	answer = correct[elem]
+	print(userAnswer, answer)
+
+	if(userAnswer == answer):
+		count += 1
+		print('same', userIntensity)
+		for intensity, x,y, in locations:
+			if intensity == userIntensity:
+				cv2.line(im, (x-100, y+25), (x-150, y-25), (0, 200, 20), 20)
+				cv2.line(im, (x-100, y+25), (x-50, y-75), (0, 200, 20), 20)
+	else:
+		for intensity, x,y, in locations:
+			if intensity == userIntensity:
+				cv2.line(im, (x-150, y-25), (x-100, y+25), (0, 20, 200), 20)
+				cv2.line(im, (x-100, y-25), (x-150, y+25), (0, 20, 200), 20)
+
+print(count*25)
+outputText = "{}%".format(count*25)
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(im, outputText, (200,450), font, 10, (255, 0, 0), 8, cv2.LINE_AA)
+# for intensity, loc in locations:
+    
+# cv2.line(im, (202, 220), (800, 400), (0, 20, 200), 10)
+# print(res)
+if res  == correct:
+    print('good')
+# plt.imshow(im)
+outputName = image + "-marked" + "." + extension
+print("writing to ", outputName)
+
+
+cv2.imwrite(outputName, im)
